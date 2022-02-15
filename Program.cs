37859508cac469bb5b96337f6d3adc2258331fc2
sofiyailina <84123253+sofiyailina@users.noreply.github.com>
2022-02-15 22:20:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Пятью_пять___двадцать_пять_
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a * a);
+        }
+    }
+}
