8230521a976a31a27189c09a888a5e713811170a
sofiyailina <84123253+sofiyailina@users.noreply.github.com>
2022-02-15 22:38:47 +0300

    Add files via upload

---
+++ Арифметика.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Арифметика
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] p = input.Split(" ");
+            int a = Convert.ToInt32(p[0]);
+            int b = Convert.ToInt32(p[1]);
+            int c = Convert.ToInt32(p[2]);
+            if (a * b = c) { Console.WriteLine("YES"); }
+            else { Console.WriteLine("NO"); }
+        }
+    }
+}
