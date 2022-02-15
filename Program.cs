90d89b9fe36f02cab2335148fd5d52cf58424c10
sofiyailina <84123253+sofiyailina@users.noreply.github.com>
2022-02-15 22:04:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Сумма
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int b = 0, b <= a, b++ )
+            {
+                sum++;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
