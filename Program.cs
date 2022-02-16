d3a4a46913ac4d587062ce18055fb21b531957ee
sofiyailina <84123253+sofiyailina@users.noreply.github.com>
2022-02-16 12:10:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace массивы
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] nums = new int[20] { 54, -21, 18, -19, 25, 60, 63, -49, -31, 75, 49, -66, 58, -2, -43, -77, 53, 32, 78, 1 };
+
+            int min = int.MaxValue;
+            for (int i = 0, i < nums.Length; i++) 
+            {
+                int i = 0;
+                if (nums[i] < min)
+                    min = nums[i];
+            } Console.WriteLine(min);
+
+            int max = int.MinValue;
+            for (int i = 0, i < nums.Length; i++) 
+            {
+                int i = 0;
+                if (nums[i] > max)
+                    max = nums[i];
+            } Console.WriteLine(max);
+
+            max = int.MinValue;
+            int max_pos = int.MinValue;
+            for (int i = 0, i <nums.Length; i++)
+            {
+                if (nums[i] > max) { max = nums[i]; max_pos = 1; }
+            } Console.WriteLine(max_pos);
+
+
+            min = int.MaxValue;
+            int min_pos = int.MaxValue;
+            for (int i = 0, i < nums.Length; i++)
+            {
+                if (nums[i] < min) { min = nums[i]; min_pos = 1; }
+            } Console.WriteLine(min_pos);
+
+
+        }
+    }
+}
