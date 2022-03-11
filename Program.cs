72ea280d7e27bb7624048a8e6306415843c9d717
sofiyailina <84123253+sofiyailina@users.noreply.github.com>
2022-03-11 23:33:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace фигура_псевдографикой
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int heigth = int.Parse(Console.ReadLine());
+            //int width = int.Parse(Console.ReadLine());
+            Console.WriteLine("hello");
+            for (int x = 0; x < heigth; x++)
+            {
+                for (int y = 0; y <= x; y++)
+                {
+                    Console.Write("@");
+                }
+                Console.WriteLine();
+            }
+            for (int x = 0; x < heigth; x++)
+            {
+                for (int y = heigth; y > x; y--)
+                {
+                    Console.Write("@");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
